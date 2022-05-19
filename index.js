ba0f3d7181e071b3c757c456a5db57ae74292745
Lucy <lucy.d.bullen@gmail.com>
2022-05-19 16:18:00 -0600

    commit tree ea4eff5bd8a44f7c2277d68ab8ac8cfcf37f03db

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function (e){
+  const textPargraph = document.querySelector('p#text');
+  textPargraph.textContent = 'This is really cool!';
+})
